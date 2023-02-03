422d1ffda375b806874dea58ddabe44b98ec8ad3
patriknyfeldt <patriknyfeldt@hotmail.com>
2023-02-03 16:00:03 +0100

    installed openApi plugin

---
+++ src/components/HomepageFeatures/Feature.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const Feature = ({Svg, title, description}) => {
+
+    return ( 
+      <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+     );
+  }
+
+  export default Feature;
---
+++ src/pages/my-react-page.jsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import Layout from '@theme/Layout';
+
+// export default function MyReactPage() {
+//   return (
+//     <Layout>
+//       <h1>My React Page</h1>
+//       <p>This is a React page</p>
+//     </Layout>
+//   );
+// }
+import React from 'react';
+import Layout from '@theme/Layout';
+
+const MyReactPage = () => {
+  return ( 
+    <Layout>
+      <h1>My React Page</h1>
+      <p>This is a React page</p>
+    </Layout>
+   );
+}
+ 
+export default MyReactPage;
